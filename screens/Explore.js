a588210a4a8bbe1b0c5d97a3ec3890a9e7f1fc73
dinhtrivonguyen <dinhtrivonguyen@gmail.com>
2020-08-30 15:40:06 +0700

    update ui search bar

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,200 +1,112 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
-  Animated,
-  Dimensions,
-  Image,
+  Text,
   StyleSheet,
-  ScrollView,
-  TouchableOpacity
-} from "react-native";
-import * as Icon from "@expo/vector-icons";
-import { LinearGradient } from "expo-linear-gradient";
+  View,
+  FlatList,
+  TextInput,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
 
-import { Button, Input, Block, Text } from "../components";
-import { theme, mocks } from "../constants";
-
-const { width, height } = Dimensions.get("window");
-
-class Explore extends Component {
-  state = {
-    searchFocus: new Animated.Value(0.6),
-    searchString: null
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: true, text: '' };
+    this.arrayholder = [];
+  }
+//import link api json to connect server. sample data api below
+  componentDidMount() {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson
+          },
+          function() {
+            this.arrayholder = responseJson;
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  SearchFilterFunction(text) {
+    const newData = this.arrayholder.filter(function(item) {
+      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+ 
+      dataSource: newData,
+      text: text,
+    });
+  }
+  ListViewItemSeparator = () => {
+    //Item sparator view
+    return (
+      <View
+        style={{
+          height: 0.3,
+          width: '90%',
+          backgroundColor: '#080808',
+        }}
+      />
+    );
   };
-
-  handleSearchFocus(status) {
-    Animated.timing(this.state.searchFocus, {
-      toValue: status ? 0.8 : 0.6, // status === true, increase flex size
-      duration: 150 // ms
-    }).start();
-  }
-
-  renderSearch() {
-    const { searchString, searchFocus } = this.state;
-    const isEditing = searchFocus && searchString;
-
-    return (
-      <Block animated middle flex={searchFocus} style={styles.search}>
-        <Input
-          placeholder="Tìm kiếm"
-          placeholderTextColor={theme.colors.gray2}
-          style={styles.searchInput}
-          onFocus={() => this.handleSearchFocus(true)}
-          onBlur={() => this.handleSearchFocus(false)}
-          onChangeText={text => this.setState({ searchString: text })}
-          value={searchString}
-          onRightPress={() =>
-            isEditing ? this.setState({ searchString: null }) : null
-          }
-          rightStyle={styles.searchRight}
-          rightLabel={
-            <Icon.FontAwesome
-              name={isEditing ? "close" : "search"}
-              size={theme.sizes.base / 1.6}
-              color={theme.colors.gray2}
-              style={styles.searchIcon}
-            />
-          }
-        />
-      </Block>
-    );
-  }
-/*
-  renderImage(img, index) {
-    const { navigation } = this.props;
-    const sizes = Image.resolveAssetSource(img);
-    const fullWidth = width - theme.sizes.padding * 2.5;
-    const resize = (sizes.width * 100) / fullWidth;
-    const imgWidth = resize > 75 ? fullWidth : sizes.width * 1;
-
-    return (
-      <TouchableOpacity
-        key={`img-${index}`}
-        onPress={() => navigation.navigate("Product")}
-      >
-        <Image
-          source={img}
-          style={[styles.image, { minWidth: imgWidth, maxWidth: imgWidth }]}
-        />
-      </TouchableOpacity>
-    );
-  }
-
-  renderExplore() {
-    const { images, navigation } = this.props;
-    const mainImage = images[0];
-
-    return (
-      <Block style={{ marginBottom: height / 3 }}>
-        <TouchableOpacity
-          style={[styles.image, styles.mainImage]}
-          onPress={() => navigation.navigate("Product")}
-        >
-          <Image source={mainImage} style={[styles.image, styles.mainImage]} />
-        </TouchableOpacity>
-        <Block row space="between" wrap>
-          {images.slice(1).map((img, index) => this.renderImage(img, index))}
-        </Block>
-      </Block>
-    );
-  }
-*/
-
-  renderFooter() {
-    return (
-      <LinearGradient
-        locations={[0.5, 1]}
-        style={styles.footer}
-        colors={["rgba(255,255,255,0)", "rgba(255,255,255,0.6)"]}
-      >
-        <Button gradient style={{ width: width / 2.678 }}>
-          <Text bold white center>
-            Tìm kiếm
-          </Text>
-        </Button>
-      </LinearGradient>
-    );
-  }
-
   render() {
+    if (this.state.isLoading) {
+
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
-      <Block>
-        <Block flex={false} row center space="between" style={styles.header}>
-          <Text h1 bold>
-            
-          </Text>
-          {this.renderSearch()}
-        </Block>
 
-
-
-        {this.renderFooter()}
-      </Block>
+      <View style={styles.viewStyle}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={text => this.SearchFilterFunction(text)}
+          value={this.state.text}
+          underlineColorAndroid="transparent"
+          placeholder="Tìm kiếm nhanh"
+        />
+        <FlatList
+          data={this.state.dataSource}
+          ItemSeparatorComponent={this.ListViewItemSeparator}
+          renderItem={({ item }) => (
+            <Text style={styles.textStyle}>{item.title}</Text>
+          )}
+          enableEmptySections={true}
+          style={{ marginTop: 10 }}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
     );
   }
 }
-/* <ScrollView showsVerticalScrollIndicator={false} style={styles.explore}>
-      {this.renderExplore()}
-  </ScrollView>
-*/
-// thuộc dòng số 126-128 để mở lại scrollview ảnh
-
-Explore.defaultProps = {
-  images: mocks.explore
-};
-
-export default Explore;
-
 const styles = StyleSheet.create({
-  header: {
-    paddingHorizontal: theme.sizes.base * 2,
-    paddingBottom: theme.sizes.base * 2
+  viewStyle: {
+    justifyContent: 'center',
+    flex: 1,
+    marginTop: 40,
+    padding: 16,
   },
-  search: {
-    height: theme.sizes.base * 10,
-    width: width - theme.sizes.base * 10
+  textStyle: {
+    padding: 10,
   },
-  searchInput: {
-    fontSize: theme.sizes.caption,
-    height: theme.sizes.base * 2,
-    backgroundColor: "rgba(142, 142, 147, 0.06)",
-    borderColor: "rgba(142, 142, 147, 0.06)",
-    paddingLeft: theme.sizes.base / 1.333,
-    paddingRight: theme.sizes.base * 1.5
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
   },
-  searchRight: {
-    top: 0,
-    marginVertical: 0,
-    backgroundColor: "transparent"
-  },
-  searchIcon: {
-    position: "absolute",
-    right: theme.sizes.base / 1.333,
-    top: theme.sizes.base / 1.6
-  },
-  explore: {
-    marginHorizontal: theme.sizes.padding * 1.25
-  },
-  image: {
-    minHeight: 100,
-    maxHeight: 130,
-    maxWidth: width - theme.sizes.padding * 2.5,
-    marginBottom: theme.sizes.base,
-    borderRadius: 4
-  },
-  mainImage: {
-    minWidth: width - theme.sizes.padding * 2.5,
-    minHeight: width - theme.sizes.padding * 2.5
-  },
-  footer: {
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    left: 0,
-    overflow: "visible",
-    alignItems: "center",
-    justifyContent: "center",
-    height: height * 0.1,
-    width,
-    paddingBottom: theme.sizes.base * 4
-  }
 });
